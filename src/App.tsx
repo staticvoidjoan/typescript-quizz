715f0e435bed4e6dff012457b0ca6669fc058072
Joan <104306249+staticvoidjoan@users.noreply.github.com>
2024-02-16 09:15:28 +0100

    Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
 import { questions } from "./Questions";
 import Question from "./components/Question";
-
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
-import "./App.css"
+import "./App.css";
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleSlideCHange = (swiper: any) => {
+    setCurrentPage(swiper.realIndex + 1);
+  };
   return (
     <main>
-      <h1>Swipe for more questions!</h1>
+      <h1 className="title">Swipe for more questions!</h1>
       <div className="main-container">
-
-      <Swiper
-        spaceBetween={1}
-        slidesPerView={1}
-        modules={[Navigation, Pagination]}
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        navigation
+        <Swiper
+          spaceBetween={1}
+          slidesPerView={1}
+          modules={[Navigation, Pagination]}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSlideChange={handleSlideCHange}
+          navigation
         >
-        {questions.map((q) => (
-          <SwiperSlide
-          key={q.id}
-          style={{ display: "flex", justifyContent: "center" }}
-          >
-            <Question question={q.question} answer={q.answer} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          {questions.map((q) => (
+            <SwiperSlide
+              key={q.id}
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              <Question question={q.question} answer={q.answer} />
+            </SwiperSlide>
+          ))}
+        <div className="pagination-info">
+          Question: {currentPage} of {questions.length}
         </div>
+        </Swiper>
+      </div>
     </main>
   );
 }
